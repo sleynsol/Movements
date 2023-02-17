3dd473347618946e3afff16ac67f15ad0f1a78b9
sleynsol <sleyn_sol@protonmail.com>
2023-02-17 17:52:35 +0100

    cleaned up web3 service

--- frontend/src/app/services/web3/web3.service.ts
+++ frontend/src/app/services/web3/web3.service.ts
@@ -2,17 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 import { Connection, PublicKey } from '@solana/web3.js';
-import { Tx } from 'src/app/model/Tx';
-import { NFTs } from 'src/app/constants/nfts';
 import { environment } from 'src/environments/environment';
-import { Transaction } from 'src/app/model/Transaction';
 import { HistoryData } from 'src/app/model/HistoryData';
 import { Token } from 'src/app/model/Token';
-
-const ADDRESS = "6MpxjLErHqDfbRMjhRepSA17p2gkWS4W7VPSEE97a9Nf"
 
 @Injectable({
   providedIn: 'root'
@@ -21,65 +14,15 @@ export class Web3Service {
 
   connection: Connection;
 
-  walletAdapter: PhantomWalletAdapter | SolflareWalletAdapter = new PhantomWalletAdapter();
-  isConnectedSubject: BehaviorSubject<boolean> = new BehaviorSubject(false)
-  isLoggedInSubject: BehaviorSubject<boolean> = new BehaviorSubject(false)
-  installedWallets: string[] = [];
-
   tokens: Token[];
   nfts: Token[];
+  publicKeySubject: BehaviorSubject<PublicKey> = new BehaviorSubject(null);
 
   constructor(private http: HttpClient,  private router: Router) {
-    this.checkInstalledWallets()
-    this.connectWallet()
   }
 
-  checkInstalledWallets() {
-    //@ts-ignore
-    if(window.phantom) this.installedWallets.push('phantom')
-    //@ts-ignore
-    if(window.solflare) this.installedWallets.push('solflare')
-  }
-
-  getInstalledWallets() {
-    return this.installedWallets;
-  }
-
-  selectWalletAdapter(adapter: string) {
-    switch(adapter) {
-      case "phantom":
-        this.walletAdapter = new PhantomWalletAdapter();
-        break;
-      case "solflare":
-        this.walletAdapter = new SolflareWalletAdapter();
-      break;
-    }
-  }
- 
-  connectWallet() {
-    this.walletAdapter.connect().then(() => {
-      this.isConnectedSubject.next(true);
-    })
-  }
-
-  disconnectWallet() {
-    this.walletAdapter.disconnect().then(() => {
-      this.isConnectedSubject.next(false);
-      this.isLoggedInSubject.next(false);
-      this.router.navigateByUrl("login");
-    })
-  }
-
-  getWeb3ConnectionSubject$(): BehaviorSubject<boolean> {
-    return this.isConnectedSubject;
-  }
-
-  getLoggedInSubject$(): BehaviorSubject<boolean> {
-    return this.isLoggedInSubject;
-  }
-
-  getPublicKey() {
-    return this.walletAdapter.publicKey
+  getPublicKeySubject$() {
+    return this.publicKeySubject;
   }
 
   getAllTransactions(publicKey: string): Observable<HistoryData> {
@@ -88,10 +31,10 @@ export class Web3Service {
       (history: HistoryData) => {
         this.tokens = history.tokens;
         this.nfts = history.nfts;
+        this.publicKeySubject.next(new PublicKey(history.publicKey));
+        
       }
     ))
-//    return TXs.filter(tx => tx.type != 'UNKNOWN');
-   // return this.http.get(`https://api.helius.xyz/v0/addresses/${ADDRESS}/transactions?api-key=${API_KEY}`)
   }
 
   getTokenMetadata(mint: string) {
@@ -102,16 +45,5 @@ export class Web3Service {
     return this.nfts.find((nft: Token) => nft.mint == mint) 
   }
 
-
-
-  /*createRpcConnection() {
-    let accessToken = this.auth.getToken()
-    this.connection = new Connection(`${environment.rpc_endpoint}`, {
-      commitment: 'confirmed',
-      httpHeaders: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
-  }*/
-
 }
+
